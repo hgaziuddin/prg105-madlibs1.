da6f6075ca10ce41decfd70ee395d6f4b1feee42
hgaziuddin <hgaziuddin@hotmail.com>
2016-09-02 15:27:27 -0500

    Add files via upload

---
+++ BABA Black Sheep.py
@@ -0,0 +1,17 @@
+noise = "Quack"
+animal = "duck"
+animalProduct = "feathers"
+person1 = "chicken"
+amount = 42
+amount2 = amount / 3
+person2 = "Donald Trump"
+noun = "trash can"
+
+print(noise + ", " + noise + ", black " + animal + ",")
+print("Have you any " + animalProduct + "?")
+print("Yes " + person1 + ", yes " + person1 + ",")
+print("4 bags full;")
+print("1 for the master,")
+print("And 1 for the dame,")
+print("And 1 for the " + person2 )
+print("Who lives in a " + noun)
