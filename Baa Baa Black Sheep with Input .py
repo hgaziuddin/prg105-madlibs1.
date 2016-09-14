6fbe98050995ab3acfd3c10712ee022d23555822
hgaziuddin <hgaziuddin@students.mchenry.edu>
2016-09-13 20:52:46 -0500

    Update and rename BABA Black Sheep.py to Baa Baa Black Sheep with Input .py

--- Baa Baa Black Sheep with Input .py
+++ Baa Baa Black Sheep with Input .py
@@ -1,21 +1,34 @@
-# User Input
-print("Please enter an onomatopoeia (First letter Uppercased Please)")
-noise = raw_input()
-print("Please enter a color")
+print("Please enter a color or adjective ")
 adj = raw_input()
-print("Please enter an animal")
+if adj == " ":
+        adj == "Black"
+
+print("Please enter a noun")
 animal = raw_input()
+if animal == " ":
+        animal == "Sheep"
+
 print("Please enter a plural noun")
 noun1 = raw_input()
-print("Please enter a proper noun")
+if noun1 == " ":
+        noun1 == "Wool"
+
+print("Please enter a proper noun or a noun")
 properNoun = raw_input()
+if properNoun == " ":
+        properNoun == "Sir"
+
 print("Please enter an integer (That is divisible by 3 please)")
 amount = raw_input()
-print("Please enter a noun")
+# Please do not leave this empty, because if you do you will get "ValueError: empty string for float()" error
+
+print("Please enter a noun or a proper noun")
 noun2 = raw_input()
+if noun2 == " ":
+       noun2 == "Little Boy"
 
 # Calculations
-amount2 = int(amount) / 3
+amount2 = float(amount) / 3
 amount2 = str(amount2)
 
 # "Madlib"
