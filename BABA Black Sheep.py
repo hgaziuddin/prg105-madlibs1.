f550ed7fbb997e36a6068204c5865fe21e8d228a
hgaziuddin <hgaziuddin@students.mchenry.edu>
2016-09-13 19:55:26 -0500

    Update BABA Black Sheep.py

--- BABA Black Sheep.py
+++ BABA Black Sheep.py
@@ -1,17 +1,29 @@
-noise = "Quack"
-animal = "duck"
-animalProduct = "feathers"
-person1 = "chicken"
-amount = 42
-amount2 = amount / 3
-person2 = "Donald Trump"
-noun = "trash can"
+# User Input
+print("Please enter an onomatopoeia (First letter Uppercased Please)")
+noise = raw_input()
+print("Please enter a color")
+adj = raw_input()
+print("Please enter an animal")
+animal = raw_input()
+print("Please enter a plural noun")
+noun1 = raw_input()
+print("Please enter a proper noun")
+properNoun = raw_input()
+print("Please enter an integer (That is divisible by 3 please)")
+amount = raw_input()
+print("Please enter a noun")
+noun2 = raw_input()
 
-print(noise + ", " + noise + ", black " + animal + ",")
-print("Have you any " + animalProduct + "?")
-print("Yes " + person1 + ", yes " + person1 + ",")
-print("4 bags full;")
-print("1 for the master,")
-print("And 1 for the dame,")
-print("And 1 for the " + person2 )
-print("Who lives in a " + noun)
+# Calculations
+amount2 = int(amount) / 3
+amount2 = str(amount2)
+
+# "Madlib"
+print("\n" + noise + ", " + noise + ", " + adj + " " + animal + ",")
+print("Have you any " + noun1 + "?")
+print("Yes " + properNoun + ", yes " + properNoun + ",")
+print(amount + " bags full;")
+print(amount2 + " for the master,")
+print("And " + amount2 + " for the dame,")
+print("And " + amount2 + " for the " + noun2)
+print("Who lives down the lane")
